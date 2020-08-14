961a57f02d745bffac9bcf41a33ff80182ea5732
Sean Kramer <seandominickramer@gmail.com>
2020-08-14 19:34:17 -0400

    Add skeleton of useForm custom hook

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { useForm } from '../hooks/useForm';
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,7 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue) => {
+  console.log(initialValue);
+  return (initialValue)
+}
