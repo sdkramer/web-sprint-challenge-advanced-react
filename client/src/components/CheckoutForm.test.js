953439f24fc17b4afe0d08fe06bcd7e2d2ef9eaa
Sean Kramer <seandominickramer@gmail.com>
2020-08-14 20:09:09 -0400

    RAdd a sample test for rendering App

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from '../App';
+
+test('renders App without crashing', () => {
+  render(<App />);
+});
 
 // Write up the two tests here and make sure they are testing what the title shows
 
