a6a7b61f8905955993989edfca2c234ff3d9bdee
Sean Kramer <seandominickramer@gmail.com>
2020-08-14 20:02:48 -0400

    Revise useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -3,5 +3,12 @@ import { useState } from 'react';
 
 export const useForm = (initialValue) => {
   console.log(initialValue);
-  return (initialValue)
+
+  const [values, setValues] = useState(initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return [ values, handleChanges ];
 }
