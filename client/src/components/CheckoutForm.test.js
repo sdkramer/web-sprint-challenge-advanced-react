7192b879bbe142cc60935bb06e70a74aedab5136
Sean Kramer <seandominickramer@gmail.com>
2020-08-14 20:19:57 -0400

    RAdd test for rendering CheckoutForm

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 import App from '../App';
 
@@ -9,6 +9,8 @@ test('renders App without crashing', () => {
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+});
 
 test("form shows success message on submit with form details", () => {});
